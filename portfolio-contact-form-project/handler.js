4ef759566879ba3580d5d07390ec140897523672
knopgm <knop.gm@gmail.com>
2022-12-27 21:56:01 +0100

    Setup a serverless function to receive messages from your portfolio contact form

---
+++ portfolio-contact-form-project/handler.js
@@ -0,0 +1,46 @@
+"use strict";
+const AWS = require("aws-sdk");
+const SES = new AWS.SES();
+
+function sendEmail(formData, callback) {
+  const emailParams = {
+    Source: "knop.gm@gmail.com", // SES SENDING EMAIL
+    ReplyToAddresses: [formData.reply_to],
+    Destination: {
+      ToAddresses: ["knop.gm@gmail.com"], // SES RECEIVING EMAIL
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: `${formData.message}\n\nName: ${formData.name}\nEmail: ${formData.reply_to}`,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Portfolio New message (https://gabriela-portfolio-cf.netlify.app)",
+      },
+    },
+  };
+
+  SES.sendEmail(emailParams, callback);
+}
+
+module.exports.staticSiteMailer = (event, context, callback) => {
+  const formData = JSON.parse(event.body);
+
+  sendEmail(formData, function (err, data) {
+    const response = {
+      statusCode: err ? 500 : 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+      },
+      body: JSON.stringify({
+        message: err ? err.message : data,
+      }),
+    };
+
+    callback(null, response);
+  });
+};
