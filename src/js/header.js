f524af4b9436bc0705e2c6b651107ef2c70cbb96
knopgm <knop.gm@gmail.com>
2023-01-05 10:02:21 +0100

    Rebuild portfolio style and organize it

---
+++ src/js/header.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+  const element = document.getElementsByClassName("navigation-list__item");
+  element.classList.toggle("navigation-list__item--active");
+}
+
+{
+  /* <ul id="menu" role="menubar" class="navigation-list">
+  <li role="menuitem">
+    <a
+      href="#welcome"
+      class="navigation-list__item navigation-list__item--active"
+    >
+      About me
+    </a>
+  </li>
+  <li role="menuitem">
+    <a href="#projects" class="navigation-list__item">
+      Projects
+    </a>
+  </li>
+  <li role="menuitem">
+    <a href="#contact" class="navigation-list__item">
+      Contact
+    </a>
+  </li>
+</ul>; */
+}
